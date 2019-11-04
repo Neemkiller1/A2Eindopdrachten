6388dbc4204cdbfd2f65b685004e11e955300db9
ErikMast <e.mast@drenthecollege.nl>
2019-11-04 16:47:43 +0100

    Aanpassing van bestanden naar de opdrachten

---
+++ src/nl/drenthecollege/modules/a2/eindopdrachten/FormuleBereken.java
@@ -0,0 +1,14 @@
+
+package nl.drenthecollege.modules.a2.eindopdrachten;
+
+/**
+ *
+ * @author Erik Mast <e.mast@drenthecollege.nl>
+ */
+class FormuleBereken {
+
+    static Integer bereken(String formule) {
+        return 0;
+    }
+    
+}
--- src/nl/drenthecollege/modules/a2/eindopdrachten/Opdracht2.java
+++ src/nl/drenthecollege/modules/a2/eindopdrachten/Opdracht2.java
@@ -1,17 +1,20 @@
+
 package nl.drenthecollege.modules.a2.eindopdrachten;
 
+/**
+ *
+ * @author Erik Mast <e.mast@drenthecollege.nl>
+ */
 public class Opdracht2 {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         char[] weg = {'"', '?'};
-        
-       String tekst = 
-    		   "Dit is een tekst met \" en ** en ?? "+
-    		   "en allerlei andere niet wenselijke tekens zoals ® etc.  ";
-       
-       System.out.println(tekst);
+        String tekst
+                = "Dit is een tekst met \" en ** en ?? "
+                + "en allerlei andere niet wenselijke tekens zoals Â® etc. ";
+        System.out.println(tekst);
     }
- 
 }
